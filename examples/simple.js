e90228f0da69855561b4e4425a97eff6a43ff777
Tom Yandell <tom@yandell.me.uk>
2012-11-24 09:58:43 +0000

    added default multiprocess mode

--- examples/simple.js
+++ examples/simple.js
@@ -7,7 +7,8 @@ module.exports.prototype.handle = function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
   response.end(
       'started: ' + started + '\n' +
-      'running: ' + (new Date() - started) + '\n'
+      'running: ' + (new Date() - started) + '\n' +
+      'pid:     ' + process.pid + '\n'
     );
 };
 
