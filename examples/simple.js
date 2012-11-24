06e67e348668474b25afabf09d69e24da91fe9a1
Tom Yandell <tom@yandell.me.uk>
2012-11-24 08:47:29 +0000

    Split servers into class hierarchy. Reordered functionality to make more sense to the reader. Added docs. Fixed reload functionality.

--- examples/simple.js
+++ examples/simple.js
@@ -1,8 +1,13 @@
 
 module.exports = function () {};
 
+var started = new Date();
+
 module.exports.prototype.handle = function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello World\n');
+  response.end(
+      'started: ' + started + '\n' +
+      'running: ' + (new Date() - started) + '\n'
+    );
 };
 
